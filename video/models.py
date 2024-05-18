c15d903b75f6d0188d3094604f0c049231408b9e
what1sloove <vladis4ulove@.com>
2024-05-18 21:48:25 +0300

    The model is registered with the administrator.

--- video/models.py
+++ video/models.py
@@ -6,3 +6,10 @@ class Video(models.Model):
     message = models.CharField(max_length=100)
     file = models.FileField(upload_to='videos/')
     create_time = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Видео'
+        verbose_name_plural = 'Видео'
